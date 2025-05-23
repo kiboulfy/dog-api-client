205d47e2b2e169da229e225484d17900c8960239
kiboulfy <shaoalex225@gmail.com>
2025-05-23 21:52:08 +0300

    improved rest parts considering review

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 from httpx import Client
-from models import BreedModel, BreedModelList, Facts, Group, Groups
 from pydantic import UUID4
+
+from .models import BreedModel, BreedModelList, Facts, Group, Groups
 
 
 class DogApiClient:
@@ -8,33 +9,27 @@ class DogApiClient:
         self.client = Client(base_url="https://dogapi.dog/api/v2")
 
     def get_breeds(self) -> BreedModelList:
-        response = self.client.get(f"{self.client.base_url}/breeds")
+        response = self.client.get("/breeds")
         return BreedModelList.model_validate_json(response.content)
 
     def get_breed(self, id: UUID4) -> BreedModel:
-        response = self.client.get(f"{self.client.base_url}/breeds/{id}")
-        return repr(BreedModel.model_validate_json(response.content))
+        response = self.client.get(f"/breeds/{id}")
+        return BreedModel.model_validate_json(response.content)
 
     def get_facts(self, limit: int | None = None) -> Facts:
         if not limit:
-            response = self.client.get(f"{self.client.base_url}/facts")
+            response = self.client.get("/facts")
         else:
-            response = self.client.get(f"{self.client.base_url}/facts?limit={limit}")
+            response = self.client.get("/facts", params={"limit": limit})
         return Facts.model_validate_json(response.content)
 
     def get_groups(self) -> Groups:
-        response = self.client.get(f"{self.client.base_url}/groups")
+        response = self.client.get("/groups")
         return Groups.model_validate_json(response.content)
 
     def get_group(self, id: UUID4) -> Group:
-        response = self.client.get(f"{self.client.base_url}/groups/{id}")
+        response = self.client.get(f"/groups/{id}")
         return Group.model_validate_json(response.content)
 
 
 client = DogApiClient()
-
-# print(client.get_breeds()) #test get_breeds
-# print(client.get_breed("8355b9c9-3724-477d-858a-c1c1c0f1743f")) #test get_breed
-# print(client.get_facts(2)) #test get_facts
-# print(client.get_groups())  # test get_groups
-# print(client.get_group("8000793f-a1ae-4ec4-8d55-ef83f1f644e5"))
---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+from src.client import DogApiClient
+
+client = DogApiClient()
+
+# print(client.get_breeds())  # test get_breeds
+# print(client.get_breed("8355b9c9-3724-477d-858a-c1c1c0f1743f"))  # test get_breed
+# print(client.get_facts(2))  # test get_facts
+# print(client.get_groups())  # test get_groups
+# print(client.get_group("8000793f-a1ae-4ec4-8d55-ef83f1f644e5")) # test get_groop
